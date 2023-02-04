d181c22b21be4c729edec3a5e456c72ca45aadb3
sandeepp873 <69438787+sandeepp873@users.noreply.github.com>
2023-02-03 19:12:05 -0500

    MongoDBConnectivity

--- src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
+++ src/main/java/com/acc/training/customerapi/repository/CustomerRepository.java
@@ -1,46 +1,11 @@
 package com.acc.training.customerapi.repository;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-import org.springframework.stereotype.Repository;
+import com.acc.training.customerapi.domain.CustomerDomain;
 
-import com.acc.training.customerapi.model.Customer;
+public interface CustomerRepository extends MongoRepository<CustomerDomain, String> {
 
-@Repository
-public class CustomerRepository {
-
-    private static final Map<String, Customer> customerDB = new HashMap<>();
-
-    static{
-        initCustomerDB();
-    }
-
-
-    
-    public Customer fetchCustomer(String id) {
-        return customerDB.get(id);
-    }
-
-     
-
-    private static void initCustomerDB() {
-        Customer cust = new Customer();
-        cust.setCustomerId("12345");
-        cust.setCustomerName("Sandeep Patil");
-        cust.setCustomerAddress("Hartford");
-        cust.setOfficeCode(BigDecimal.valueOf(45));
-
-        customerDB.put(cust.getCustomerId(), cust);
-
-
-    }
-
-
-
-    public Customer saveCustomer(Customer requestBody) {
-        return customerDB.put(requestBody.getCustomerId(),requestBody);
-    }
+    public CustomerDomain findByCustomerId(String customerId);
     
 }
